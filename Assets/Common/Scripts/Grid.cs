ef7e0bb1f7c30672ccd7d005a335ad87a5617d7e
inexts1996 <inexts0618@gmail.com>
2025-02-06 18:38:19 +0800

    learn procedural grid programming form

---
+++ Assets/Common/Scripts/Grid.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class Grid : MonoBehaviour
+{
+    [SerializeField]
+    private int xSize = 10;
+    [SerializeField]
+    private int ySize = 5;
+    private Vector3[] vertices;
+    private void Awake()
+    {
+        Generate();
+    }
+
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        Generate();
+    }
+#endif
+
+    private void Generate()
+    {
+        vertices = new Vector3[(xSize + 1) * (ySize + 1)];
+
+        // 计算网格顶点坐标（基于对象本地坐标系）
+        for (int y = 0, i = 0; y <= ySize; y++)
+        {
+            for (int x = 0; x <= xSize; x++, i++)
+            {
+                vertices[i] = new Vector3(x, y, 0);
+            }
+        }
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        if (vertices == null) return;
+        Gizmos.color = Color.black;
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            Gizmos.DrawSphere(vertices[i], 0.1f);
+        }
+    }
+}
